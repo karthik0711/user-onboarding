5eb8422df7ad0e94c757cdef9fdd77b7523fd457
karthik <karthiksabbavarapu10@gmail.com>
2025-03-24 11:26:55 +0530

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/example/user_onboarding/UserOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserOnboardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/user_onboarding/UserOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
